7f71fb6922c0c96ae21a0dddfe02ce8572618ff4
Kartik Anand Pant <kpant14@gmail.com>
2023-10-22 13:17:25 -0400

    change folder for px4

--- get_src.sh
+++ get_src.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-if [ ! -d ./work/px4 ] ; then
-    cd ./work
-    git clone git@github.com:kpant14/PX4-Autopilot.git -b gz-gps px4
-    cd px4
-    git tag v1.14.0-beta2
-    cd ../..
-fi
-
 if [ ! -d ./work/ros2_ws/src ] ; then
     mkdir -p ./work/ros2_ws/src
     cd work/ros2_ws/src
@@ -15,6 +6,14 @@ if [ ! -d ./work/ros2_ws/src ] ; then
     git clone git@github.com:PX4/px4_msgs.git
     git clone git@github.com:kpant14/px4-offboard.git
     git clone -b humble git@github.com:gazebosim/ros_gz.git
+    cd ..
+fi
+
+#!/bin/bash
+if [ ! -d ./work/ros2_ws ] ; then
+    git clone git@github.com:kpant14/PX4-Autopilot.git -b gps_spoofing px4
+    cd px4
+    git tag v1.14.0-beta2 v1.14.0-rc2
 fi
 
 
