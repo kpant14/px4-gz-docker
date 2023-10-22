c7731c507d828e1f2fcf9a55ac47ae5363e87f91
Kartik Anand Pant <kpant14@gmail.com>
2023-10-22 15:04:58 -0400

    Added px4_gps to get_src

--- get_src.sh
+++ get_src.sh
@@ -3,9 +3,10 @@ if [ ! -d ./work/ros2_ws/src ] ; then
     cd work/ros2_ws/src
     wget https://raw.githubusercontent.com/kpant14/px4-gz-docker/gps_spoofing/collection-garden.yaml -O collection-garden.yaml
     vcs import < collection-garden.yaml
-    git clone git@github.com:PX4/px4_msgs.git
+    git clone -b main git@github.com:kpant14/px4_msgs.git
+    git clone -b ros2 git@github.com:kpant14/ros_gz.git
     git clone git@github.com:kpant14/px4-offboard.git
-    git clone -b humble git@github.com:gazebosim/ros_gz.git
+    git clone git@github.com:kpant14/px4_gps.git
     cd ..
 fi
 
@@ -13,7 +14,7 @@ fi
 if [ ! -d ./work/ros2_ws ] ; then
     git clone git@github.com:kpant14/PX4-Autopilot.git -b gps_spoofing px4
     cd px4
-    git tag v1.14.0-beta2 v1.14.0-rc2
+    git tag v1.14.0-rc2
 fi
 
 
