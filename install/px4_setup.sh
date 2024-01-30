752c70f6e10a652690c59ecf7b27d000373fecf4
mhcho1994 <mhcho01994@gmail.com>
2024-01-30 10:56:14 -0500

    Typo correction, PX4 dependencies repo from Kartiks forked version

--- install/px4_setup.sh
+++ install/px4_setup.sh
@@ -2,8 +2,8 @@
 set -e
 set -x
 
-wget https://raw.githubusercontent.com/PX4/PX4-Autopilot/main/Tools/setup/ubuntu.sh
-wget https://raw.githubusercontent.com/PX4/PX4-Autopilot/main/Tools/setup/requirements.txt
+wget https://raw.githubusercontent.com/kpant14/PX4-Autopilot/main/Tools/setup/ubuntu.sh
+wget https://raw.githubusercontent.com/kpant14/PX4-Autopilot/main/Tools/setup/requirements.txt
 bash ubuntu.sh --no-sim-tools && rm ubuntu.sh
 
 git clone https://github.com/eProsima/Micro-XRCE-DDS-Agent.git
