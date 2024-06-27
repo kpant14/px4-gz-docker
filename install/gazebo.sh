4c2de5d7e562fbfb8499486d695c45a986e22f27
mhcho1994 <mhcho01994@gmail.com>
2024-06-27 14:27:20 -0400

    working...

--- install/gazebo.sh
+++ install/gazebo.sh
@@ -2,12 +2,18 @@
 set -e
 set -x
 
-GAZEBO_VERSION="garden"
-
+sudo apt install libeigen3-dev
 sudo wget https://packages.osrfoundation.org/gazebo.gpg -O /usr/share/keyrings/pkgs-osrf-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/pkgs-osrf-archive-keyring.gpg] http://packages.osrfoundation.org/gazebo/ubuntu-stable $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/gazebo-stable.list > /dev/null
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo DEBIAN_FRONTEND=noninteractive apt-get install --no-install-recommends -y \
-	gz-${GAZEBO_VERSION}
-sudo apt install -y libignition-gazebo6-dev
+	python3-vcstool python3-colcon-common-extensions libgz-transport12-dev gz-transport12-cli
+cd /tmp
+
+wget https://raw.githubusercontent.com/kpant14/px4-gz-docker/multi_agent/repos.yaml -O repos.yaml
+vcs import < repos.yaml
+
+sudo apt-get -y install \
+  $(sort -u $(find . -iname 'packages-'`lsb_release -cs`'.apt' -o -iname 'packages.apt' | grep -v '/\.git/') | sed '/gz\|sdf/d' | tr '\n' ' ')
--- script/get_src.sh
+++ script/get_src.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# show commands before execution 
+set -e
+
 if [ ! -d ./work/px4 ] ; then
     cd ./work
     git clone git@github.com:kpant14/PX4-Autopilot.git -b gz-gps px4
@@ -11,7 +15,7 @@ if [ ! -d ./work/ros2_ws/src ] ; then
     mkdir -p ./work/ros2_ws/src
     cd work/ros2_ws/src
     git clone git@github.com:PX4/px4_msgs.git
-    git clone git@github.com:kpant14/px4-offboard.git
+    # git clone git@github.com:kpant14/px4-offboard.git
     # git clone -b humble git@github.com:gazebosim/ros_gz.git
     # cd ros_gz
     # git checkout 0.246.0
